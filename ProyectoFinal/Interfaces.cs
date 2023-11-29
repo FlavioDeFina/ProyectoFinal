398a592adc75f6cbdfbc7b64e1801ba2c84856d6
Giacomo <giacomolopezupn@gmail.com>
2023-11-29 16:38:10 -0500

    Prueba

--- ProyectoFinal/Interfaces.cs
+++ ProyectoFinal/Interfaces.cs
@@ -50,7 +50,12 @@ namespace ProyectoFinal
             {
 
             }
+            
 
+        }
+        public static void agregarProducto ()
+        {
+            Console.Write("alonso main edgar");
         }
     }
 }
