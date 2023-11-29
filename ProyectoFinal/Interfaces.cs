3f8dd106ed9046e797fbb983bb77fc1c416ebeb1
Daniel Herrera <danielfer1904@gmail.com>
2023-11-28 22:13:15 -0500

    Pantalla de Gestionar productos

--- ProyectoFinal/Interfaces.cs
+++ ProyectoFinal/Interfaces.cs
@@ -31,7 +31,6 @@ namespace ProyectoFinal
             }
 
         }
-
         public static void PantallaGestionarProductos()
         {
             string txt = "--------------------------------------------------" +
