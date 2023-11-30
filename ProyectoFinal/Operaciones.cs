34643f68855b73633ad9293a0b65ac1e0805c51d
Alonso De fina <alonso07121@gmail.com>
2023-11-30 17:34:58 -0500

    Actualizacion para ordenamiento

--- ProyectoFinal/Operaciones.cs
+++ ProyectoFinal/Operaciones.cs
@@ -8,56 +8,6 @@ namespace ProyectoFinal
 {
     internal class Operaciones
     {
-        public static int getEntero(string prefijo, string reemplazo)
-        {
-            int respuesta = 0;
-            bool correcto = false;
-
-            do
-            {
-                string numeroTexto = getTextoPantalla(prefijo);
-                correcto = int.TryParse(numeroTexto, out respuesta);
-
-                if (!correcto)
-                {
-                    Console.Clear();
-                    Console.WriteLine(reemplazo);
-                    Console.WriteLine("Ingresa un número válido");
-                }
-
-            } while (!correcto);
-
-            return respuesta;
-        }
-
-        public static float getDecimal(string prefijo, string remplazo)
-        {
-            float respuesta = 0;
-            bool correcto = false;
-
-            do
-            {
-                string numeroTexto = getTextoPantalla(prefijo);
-
-                correcto = float.TryParse(numeroTexto, out respuesta);
-
-                if (!correcto)
-
-                {
-                    Console.WriteLine("Ingresa un número válido");
-                }
-
-            } while (!correcto);
-
-            return respuesta;
-        }
-
-        public static string getTextoPantalla(string prefijo)
-
-        {
-            Console.Write(prefijo);
-
-            return Console.ReadLine();
-        }
+        
     }
 }
